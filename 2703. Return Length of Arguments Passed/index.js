96883ef6a2b51688115213d9b64d58a58c015c0a
ansorokey <ansorokey@gmail.com>
2023-10-27 06:43:33 -0400

    added some additional comments

--- 2703. Return Length of Arguments Passed/index.js
+++ 2703. Return Length of Arguments Passed/index.js
@@ -4,6 +4,9 @@
  */
 var argumentsLength = function(...args) {
 	// a function that only needs to count the number of args passed in to the function
+    // when '...' is used in the parameter list, it represents a variable number of args
+    // could be 0, could be 100
+    // can only come at the end of the parameter list, passed into function as an array
 
     let numArgs = 0;
 

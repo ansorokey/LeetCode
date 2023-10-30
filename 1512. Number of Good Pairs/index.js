57b6cfa2e69c71db9d2b1efe3b273897138c58f0
ansorokey <ansorokey@gmail.com>
2023-10-30 14:29:24 -0400

    slow but working js solution

--- 1512. Number of Good Pairs/index.js
+++ 1512. Number of Good Pairs/index.js
@@ -1,34 +1,39 @@
 var numIdenticalPairs = function(nums) {
-    //  we want to track and return the number of good pairs
-
     let output = 0;
-
-    //a good pair is a number that occurs more than once, and the index of the first is less than the index of the next
-    // Let's not focus on the index too much since if the number occurs more than once,
-    //the next index MUST be higher than the last
-    //so our first task is to iterate through the nums and keep track of each occurance
 
     let valOccur = {};
 
     for(let i = 0; i < nums.length; i++) {
         let cur = nums[i];
+        // console.log(cur);
 
         if(cur in valOccur) {
             valOccur[cur] += 1;
         } else {
-            valOccur[cur] += 1;
+            valOccur[cur] = 1;
         }
     }
 
-    //now we have a map of each value and the times they appeared in nums
-    //base don the examples provides,
-    //1 - 0 pairs
-    //1 1 - 1 pair
-    //1 1 1 - 3 pair
-    // 1 1 1 1 6 pair
-    5 -> 10
-    // the number of pairs appears to be the unique number of combinations
+    function reverseFactorial(num) {
+        let output = 0;
+        for(let i = num; i > 1; i--) {
+            output += i - 1;
+        }
+        return output;
+    }
 
+    for(key in valOccur) {
+        if(valOccur[key] >= 2) {
+            output += reverseFactorial(valOccur[key]);
+        }
+    }
 
     return output;
-  };
+};
+
+let tests = [
+[1,2,3,1,1,3],
+[1,1,1,1],
+[1,2,3]]
+
+tests.forEach(t => console.log(numIdenticalPairs(t)));

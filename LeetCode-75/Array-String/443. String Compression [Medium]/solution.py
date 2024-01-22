f8e081b85bd5180048d973fa08618f8e77f8f869
Anton Sorokey <ansorokey@gmail.com>
2024-01-22 09:36:17 -0500

    py solution 443

--- LeetCode-75/Array-String/443. String Compression [Medium]/solution.py
+++ LeetCode-75/Array-String/443. String Compression [Medium]/solution.py
@@ -82,17 +82,80 @@
 #     print(chars)
 #     return p
 
-# print(foo(["a"])) # 1
-# print(foo(["a", "a"])) # 2
-# print(foo(["a", "a", "a"])) # 2
-# print(foo(["a", "a", "b"])) # 3
-# print(foo(["a","a","b","b","c","c","c"])) # 6
-# print(foo(["a","b","b","b","b","b","b","b","b","b","b","b","b"])) # 12
+# This is the working solution
+def foo(chars):
+    # Since we're updating the input, we need to keep track of the
+    # indexes we're updating
+    p = 0
+
+    # we also need to keep track of the length of 
+    # consecutive characters
+    l = 0
+
+    # start that comparison character at the first
+    cur = chars[0]
+
+    # iterate through the characters
+    for i in range(len(chars)):
+        # if the current character is a match, inc length
+        if chars[i] == cur:
+            l += 1
+
+        # if we find a new character
+        if chars[i] != cur:
+            # overwrite the input array with the current
+            # char at the pointer, inc pointer
+            chars[p] = cur
+            p += 1
+
+            # if the length is more than 1 digit, split it apart
+            if l > 9:
+                splitL = [*str(l)]
+                for n in splitL:
+                    chars[p] = n
+                    p += 1
+            # otherwise, add it if greater than 1
+            elif l > 1:
+                chars[p] = str(l)
+                p += 1
+
+            # reset the tracking variables
+            # start l at 1 this time because when we first set the cur char,
+            # we still had to iterate over that index
+            # here, we are moving on to the next one already
+            cur = chars[i]
+            l = 1
+        
+        # if we've come across the last character, 
+        # there will be no other character to comapre to
+        # so we need to repeat the above process
+        if i+1 == len(chars):
+            chars[p] = cur
+            p += 1
+            
+            if l > 9:
+                splitL = [*str(l)]
+                for n in splitL:
+                    chars[p] = n
+                    p += 1
+            elif l > 1:
+                chars[p] = str(l)
+                p += 1
+
+    # print(chars, p)
+    return p
+
+print(foo(["a"])) # 1
+print(foo(["a", "a"])) # 2
+print(foo(["a", "a", "a"])) # 2
+print(foo(["a", "a", "b"])) # 3
+print(foo(["a","a","b","b","c","c","c"])) # 6
+print(foo(["a","b","b","b","b","b","b","b","b","b","b","b","b"])) # 12
 # print(foo(['a', 'b', 'c'])) # 3
-# print(foo(["a","a","a","a","a","b"])) # 3
+print(foo(["a","a","a","a","a","b"])) # 3
 
 
 # Submission:
-# Time:
-# Space:
-# Runtime:
+# Time: 60ms - 70.70%
+# Space: 16.8 - 59.88%
+# Runtime: best case, O(n) time, I think this is constant in space?

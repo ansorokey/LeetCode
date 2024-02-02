5e4e75495fced3250ac799ac259069b337cf6737
Anton Sorokey <ansorokey@gmail.com>
2024-02-02 09:04:09 -0500

    submission and runtime comments for py 1493

--- LeetCode-75/Sliding-Window/1493. Longest Subarray of 1's After Deleting One Element [Medium]/solution.py
+++ LeetCode-75/Sliding-Window/1493. Longest Subarray of 1's After Deleting One Element [Medium]/solution.py
@@ -12,7 +12,7 @@ def foo(nums):
             deleted -= 1
 
         # this will only activate if the above happened, 
-        # meaning at least two 0's have ben reached
+        # meaning at least two 0's have been reached
         while deleted < 0:
             # move the start past the first 0
             start += 1
@@ -35,6 +35,9 @@ print(foo([0, 1, 1])) # expect 2
 print(foo([1, 0, 1])) # expect 2
 
 # Submission:
-# Time:
-# Space:
-# Runtime:
+# Time: 465ms - 68.49%
+# Space: 20.2mb - 55.82%
+# Runtime: O(n) now that I think about it? Actually, no.
+# iterating throuh the list is On. If every elememnt was a 0, we'd be
+# advancing start by 1 every step.
+# AT MOST that could onl ever be 2n, which is still On reduced

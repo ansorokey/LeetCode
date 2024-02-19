e2d131f8c50f25ca58275b20b0fc62bc8b1e616c
Anton Sorokey <ansorokey@gmail.com>
2024-02-19 15:17:26 -0500

    renamed working solution, removed old

--- LeetCode-75/Stack/394. Decode String [Medium]/solution.py
+++ LeetCode-75/Stack/394. Decode String [Medium]/solution.py
@@ -1,82 +1,42 @@
 def foo(s):
-    numStack = []
-    stringStack = []
     curNum = []
-    curString = []
+    curSring = []
     res = []
-    prefix = []
-    
-    # since numbers can be greater than 9, we need some way of concatenating multidigit numbers
-    # let's make a set of digits as strings, since we cant check the int() of a character
+
     nums = set()
-    for i in range(9):
+    for i in range(10):
         nums.add(str(i))
 
-    # iterate through input
-    for c in s:
+    for i in range(len(s)):
+        c = s[i]
+        # print(c)
+
         if c in nums:
             curNum.append(c)
         elif c == '[':
-            # start of a new set
-            # reset current string and current number
-            numStack.append(int(''.join(curNum)))
+            res.append(''.join(curNum))
             curNum = []
 
-            if len(curString) > 0:
-                prefix.append(''.join(curString))
-                curString = []
-            else:
-                pass
+            res.append(c)
+            # print(res)
+        elif c == ']':
+            j = len(res) - 1
+            cat = []
+            while res[j] != '[':
+                j -= 1
+                cat.append(res.pop())
+            res.pop()
 
-        elif c == ']': # end of a set
-            # Number to multiply, default to 1
-            lastum = 1
-            if len(numStack) > 0:
-                lastNum = numStack.pop()
-            
-            # string to multiply, can be current set contents or the last string
-            toMultiply = ''
-            if len(curString) > 0:
-                toMultiply = ''.join(curString)
-            else:
-                # toMultiply = ''.join(res)
-                # res = []
-                toMultiply = res.pop()
+            toMultiply = ''.join(cat[::-1])
+            # print(res)
+            lastNum = int(res.pop())
+            res.append(lastNum * toMultiply)
 
-            newString = lastNum * toMultiply
-
-            if len(prefix) > 0:
-                newString = prefix.pop() + newString
-
-            # print(newString)
-            res.append(newString)
-            print(res)
-
-            curString = []
         else:
-            curString.append(c)
-        
+            res.append(c)
+
         # print(res)
 
-    if len(curString):
-        res.append(''.join(curString))
-
     return ''.join(res)
-        
-# "3[a]2[bc]"
-# "3[a2[c]]"
-# "2[abc]3[cd]ef"
-# "abc3[cd]xyz"
-# print(foo('a2[b]'))
-# print(foo('3[ab]'))
-# print(foo('2[a]2[b]'))
-# print(foo("3[a2[c]]"))
-# print(foo("abc3[de]xyz"))
-# print(foo('3[2[a]2[b]]'))
-# print(foo("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
-print(foo('2[jk]e1[f]'))
-print(foo('pq4[2[jk]e1[f]]'))
-# Submission:
-# Time:
-# Space:
-# Runtime:
+
+print(foo('10[a]10[b]10[c]'))

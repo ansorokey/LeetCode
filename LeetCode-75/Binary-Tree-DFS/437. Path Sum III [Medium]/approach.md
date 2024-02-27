101d796b59ad930637a624c195c033324474a905
Anton Sorokey <ansorokey@gmail.com>
2024-02-27 15:27:41 -0500

    py 437 solution

--- LeetCode-75/Binary-Tree-DFS/437. Path Sum III [Medium]/approach.md
+++ LeetCode-75/Binary-Tree-DFS/437. Path Sum III [Medium]/approach.md
@@ -7,3 +7,7 @@ The trick here is that the sum can start and end anywhere. It can be in the midd
 So that means that at any point in the tree, if this node plus the children node of one path equal the target, a count goes up.
 
 So we should make a recursive dfs helper.
+
+Alright, we've come back after a little break.
+After solving path sum 1 an 2, I've come to a different idea.
+The trouble I was originall having with this problem is how to start a new sum with each child node, that way a path could be found in the middle of a tree. This means a variable number of running sums. I was trying to solve this with only one running sum, where I should maybe be keeping track of all previous running sums up until the current node, and creating a new one with the next traversal. Then, at each node, I should be iterating through the given sums and test if that previous sum plus current value is equal to the target, and increment a global counter.
--- LeetCode-75/Binary-Tree-DFS/437. Path Sum III [Medium]/solution.py
+++ LeetCode-75/Binary-Tree-DFS/437. Path Sum III [Medium]/solution.py
@@ -1,7 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        res = {
+            'count': 0
+        }
+        
+        def dfs(node, prevSums=[]):
+            if node == None:
+                return
+            
+            for i in range(len(prevSums)):
+                prevSums[i] += node.val
+                if prevSums[i] == targetSum:
+                    res['count'] += 1
+                    
+            if node.val == targetSum:
+                res['count'] += 1
 
+            dfs(node.left, prevSums + [node.val]) 
+            dfs(node.right, prevSums + [node.val])
+                    
+        
+        dfs(root)
+        
+        return res['count']
 
 
 # Submission:
-# Time:
-# Space:
+# Time: 245ms - 38.83%
+# Space: 43.8mb - 7.87%
 # Runtime:
+# NEED COMMENTS
